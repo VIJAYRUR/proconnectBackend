14054772f8e9d936f7ad49951e395a8acddcd54f
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2023-09-19 11:37:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const cors = require("cors");
 const PORT = 5000;
 
 app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      next();
-    });
+  res.header('Access-Control-Allow-Origin', 'https://proconnect-frontend-git-main-vijayrur.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Add any other HTTP methods you need
+
+  next();
+});
+
 app.use(express.json());
-// app.use(cors(corsOptions));
 
 // collections
 const { user_connectDB } = require("./routes/DB/user_db");
