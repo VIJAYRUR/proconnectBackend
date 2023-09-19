f4df80f538f67743f786ff0721957272c30bec7c
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2023-09-19 11:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const PORT = 5000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://proconnect-frontend-git-main-vijayrur.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://proconnect-frontend-git-main-vijayrur.vercel.app/*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Add any other HTTP methods you need
 
