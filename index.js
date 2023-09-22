7e9f50bb844761d2b3f8081ba9c055db139b9372
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2023-09-22 14:27:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = 5000;
 // });
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000', 'https://proconnect-frontend-5pueu869d-vijayrur.vercel.app/','https://proconnect-frontend-i41l-fuqasil3p-vijayrur.vercel.app','https://proconnect-frontend.vercel.app','https://proconnect-frontend-13ovwl20y-vijayrur.vercel.app'];
+  const allowedOrigins = ['http://localhost:3000', 'https://proconnect-frontend-5pueu869d-vijayrur.vercel.app','https://proconnect-frontend-i41l-fuqasil3p-vijayrur.vercel.app','https://proconnect-frontend.vercel.app','https://proconnect-frontend-13ovwl20y-vijayrur.vercel.app'];
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
